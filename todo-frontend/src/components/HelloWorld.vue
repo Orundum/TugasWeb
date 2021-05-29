dc9c2062780a1d2aac35b14bf7b31572c78ec5de
BukanNathan <76737883+BukanNathan@users.noreply.github.com>
2021-05-29 22:37:08 +0700

    update todo front end

---
+++ todo-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- Apa kabar {{nama}}<br>
+    Nilai anda {{nilai}}<br>
+    <button @click="nilai++">tambah nilai</button>
+    <form action="/todo" method="POST">
+        <input type="text" id="deskripsi" name="deskripsi" />
+        <button @click="add">Add</button>
+    </form> -->
+    <h1>Selamat Datang</h1>
+    <div>Berikut daftar kerja kita</div>
+    <ul>
+      <li v-for="item in todos">{{item.desc}}</li>
+    </ul>
+    <input v-model="myText"/>
+    <button @click="tambahkan">Add</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    data:()=>{
+      return{
+        // nilai: 0,
+        // nama: 'Budi'
+        todos : [
+          {desc : "Makan Enak"},
+          {desc : "Makan Sayur"}
+        ],
+        myText : ' '
+      }
+    },
+    
+    methods:{
+      // addAll: function(){
+      //   this.nilai++
+      // },
+      tambahkan:function(){
+        this.todos.push ({desc : this.myText})
+      }
+    }
+  }
+</script>
